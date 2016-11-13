bef7afa90e8941bdfbf64da1027d8a76748e8a68
Ilham Mahadika <ilhamadika@students.telkomuniversity.ac.id>
2016-11-13 18:01:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/zeilin/myapplication/Bangunan.java
@@ -0,0 +1,18 @@
+package com.example.zeilin.myapplication;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ilham on 13/11/2016.
+ */
+
+public class Bangunan extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bangunan);
+    }
+
+}
---
+++ app/src/main/java/com/example/zeilin/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.zeilin.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity{
+    private EditText editEmail;
+    private EditText editPassword;
+    private Button btnLogin;
+    String username,pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+
+    private void initUI() {
+        editEmail = (EditText)findViewById(R.id.edtEmail);
+        editEmail.getText();
+        editPassword = (EditText)findViewById(R.id.edtPassword);
+        editPassword.getText();
+        btnLogin=(Button)findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v==btnLogin){
+                    if (editEmail.getText().toString().equals(username)&&
+                            editPassword.getText().toString().equals(pass)){
+                        Toast.makeText(MainActivity.this, "MainActivity Berhasil", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(MainActivity.this, "MainActivity Gagal", Toast.LENGTH_SHORT).show();
+                    }
+                    }
+                    }
+        });
+        username="banyumas";
+        pass="banyumas";
+        }
+}
